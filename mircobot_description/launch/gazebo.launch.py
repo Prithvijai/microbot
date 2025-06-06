8189a178a956397cd617af87c0e930a8e1533b65
Prithvijai <prithvijai0510@gmail.com>
2025-06-06 16:24:11 -0700

    changed code for new gazebo

--- mircobot_description/launch/gazebo.launch.py
+++ mircobot_description/launch/gazebo.launch.py
@@ -1,9 +1,9 @@
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource 
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 import os
 import xacro
 from ament_index_python.packages import get_package_share_directory
@@ -14,23 +14,53 @@ def generate_launch_description():
     display_launch = IncludeLaunchDescription(
                     PythonLaunchDescriptionSource([os.path.join(
                         get_package_share_directory("mircobot_description"),'launch','rsp.launch.py'
-                    )]), launch_arguments={'use_sim_time': 'true','use_ros2_control': 'false'}.items()  # for gazebo simulations only is the better to use gazebo plugin so better movement. use ros2_control for real-robot integration. 
+                    )]), launch_arguments={'use_sim_time': 'true','use_ros2_control': 'true'}.items()  # for gazebo simulations only is the better to use gazebo plugin so better movement. use ros2_control for real-robot integration. 
         )
-    gazebo_params_path = os.path.join(
-                  get_package_share_directory("mircobot_description"),'config','gazebo_params.yaml')
+    # gazebo_params_path = os.path.join(
+    #               get_package_share_directory("mircobot_description"),'config','gazebo_params.yaml')  old gazebo use this. 
+
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory("mircobot_description"),'launch','joystick.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    twist_mux_params = os.path.join(get_package_share_directory("mircobot_description"),'config','twist_mux.yaml')
+    twist_mux = Node(
+            package="twist_mux",
+            executable="twist_mux",
+            parameters=[twist_mux_params, {'use_sim_time': True}],
+            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
+        )
+
+    
+    default_world = os.path.join(
+        get_package_share_directory("mircobot_description"),
+        'worlds',
+        'empty.world'
+        )    
+    
+    world = LaunchConfiguration('world')
+
+    world_arg = DeclareLaunchArgument(
+        'world',
+        default_value=default_world,
+        description='World to load'
+        )
     
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py'
-                    )]), launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_path }.items()
+                    get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
+                    )]), launch_arguments={'gz_args': ['-r v4 ', world], 'on_exit_shutdown': 'true' }.items()
              )
 
     urdf_spawn_node = Node(
-        package='gazebo_ros',
-        executable='spawn_entity.py',
+        package='ros_gz_sim',
+        executable='create',
         arguments=[
-            '-entity', 'mircobot',
-            '-topic', 'robot_description'
+            '-name', 'mircobot',
+            '-topic', 'robot_description',
+            '-z', '0.1'
         ],
         output='screen'
     )
@@ -47,10 +77,34 @@ def generate_launch_description():
         arguments=['joint_broad']
     )
 
+    bridge_params = os.path.join(get_package_share_directory("mircobot_description"),'config','gz_bridge.yaml')
+
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ]
+    )
+
+    ros_gz_image_bridge = Node(
+        package="ros_gz_image",
+        executable="image_bridge",
+        arguments=["/camera/image_raw"]
+    )
+
+
     return LaunchDescription([
         display_launch,
+        joystick,
+        twist_mux,
+        world_arg,
         gazebo,
         urdf_spawn_node,
         diff_drive,
-        joint_broad
+        joint_broad,
+        ros_gz_bridge,
+        ros_gz_image_bridge
     ])
